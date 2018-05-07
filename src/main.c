994878b6ed6aaf424d397ebae8dd5e6261c9526a
damody <liam.huang@4ipnet.com>
2018-05-07 10:47:17 +0800

    add uci compile support  Changes to be committed: 	modified:   CMakeLists.txt 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -34,6 +34,51 @@
 #endif
 #include <curl/multi.h>
 
+#include <uci.h>
+
+#define UCI_CONFIG_FILE "/etc/config/4ipnet"
+static struct uci_context * ctx = NULL;
+
+bool load_config()
+{
+    struct uci_package * pkg = NULL;
+    struct uci_element *e;
+    char *tmp;
+    const char *value;
+
+    ctx = uci_alloc_context(); // 申请一个UCI上下文.
+    if (UCI_OK != uci_load(ctx, UCI_CONFIG_FILE, &pkg))
+        goto cleanup; //如果打开UCI文件失败,则跳到末尾 清理 UCI 上下文.
+
+    /*遍历UCI的每一个节*/
+    uci_foreach_element(&pkg->sections, e)
+    {
+        struct uci_section *s = uci_to_section(e);
+        printf("section s's type is %s.\n",s->type);
+        if(!strcmp("meter",s->type)) //this section is a meter
+        {
+            printf("this seciton is a meter.\n");
+            if (NULL != (value = uci_lookup_option_string(ctx, s, "modbus_id")))
+            {
+                    tmp = strdup(value); //如果您想持有该变量值，一定要拷贝一份。当 pkg销毁后value的内存会被释放。
+                    printf("%s's modbus_id is %s.\n",s->e.name,value);
+            }
+            if (NULL != (value = uci_lookup_option_string(ctx, s, "num_attr")))
+            {
+                    tmp = strdup(value); //如果您想持有该变量值，一定要拷贝一份。当 pkg销毁后value的内存会被释放。
+                    printf("%s's num_attr is %s.\n",s->e.name,value);
+            }
+        }
+        // 如果您不确定是 string类型 可以先使用 uci_lookup_option() 函数得到Option 然后再判断.
+        // Option 的类型有 UCI_TYPE_STRING 和 UCI_TYPE_LIST 两种.
+    }
+    uci_unload(ctx, pkg); // 释放 pkg
+cleanup:
+    uci_free_context(ctx);
+    ctx = NULL;
+}
+
+
 static const char *urls[] = {
   "http://www.microsoft.com",
   "http://www.opensource.org",
